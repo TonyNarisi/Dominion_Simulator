39516e4cc001222dbdecee8a58ca9225368c93b7
Tony Narisi <tonynarisi@gmail.com>
2016-06-17 01:03:36 -0400

    Add silver and gold cards

---
+++ cards/gold.rb
@@ -0,0 +1,7 @@
+# When a new instance of the gold class is created, it has a worth of 3 treasure.
+
+class Gold
+  def initialize
+    @worth = 3
+  end
+end
---
+++ cards/silver.rb
@@ -0,0 +1,7 @@
+# When a new instance of the silver class is created, it has a worth of 2 treasure.
+
+class Silver
+  def initialize
+    @worth = 2
+  end
+end
